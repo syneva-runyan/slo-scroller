0370a00874522301595e309524136c985c333662
Syneva Runyan <syneva@gmail.com>
2026-04-27 15:02:52 -0800

    remove unused param for getTrackMenuItems

--- src/game/LevelManager.js
+++ src/game/LevelManager.js
@@ -20,7 +20,7 @@ export class LevelManager {
     return this.levels[this.currentIndex];
   }
 
-  getTrackMenuItems(experimentMode = false) {
+  getTrackMenuItems() {
     return this.levelTracks.map((track) => ({
       id: track.id,
       label: track.label,
